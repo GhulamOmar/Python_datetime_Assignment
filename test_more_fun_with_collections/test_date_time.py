3054abbfda1d1030e9d0e0e9eb4e6e93817db8f4
Omar <gomar@dmacc.edu>
2019-10-15 20:58:21 -0500

    Final test

--- test_more_fun_with_collections/test_date_time.py
+++ test_more_fun_with_collections/test_date_time.py
@@ -2,12 +2,15 @@ import datetime
 import unittest
 from more_fun_with_collections import date_time
 
+""" This is a test file for date_time.py file"""
+
 
 class MyTestCase(unittest.TestCase):
     def test_half_birthday(self):
+        """ tests half_birthday."""
         birthday = datetime.date(2020, 1, 1)
-        later_half_birthday = datetime.date(2020, 7, )
-        self.assertEqual(date_time.half_birthday(birthday), later_half_birthday )
+        later_half_birthday = datetime.date(2020, 7, 1)
+        self.assertEqual(date_time.half_birthday(birthday), later_half_birthday)
 
 
 if __name__ == '__main__':
